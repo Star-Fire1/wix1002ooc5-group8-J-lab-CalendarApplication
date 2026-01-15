922fe6c5f3bd8ff20a4e41cc7640ad34eaf2eccc
CHENTIANLIN-2025 <24057231@siswa.um.edu.my>
2026-01-15 14:00:58 +0800

    Update Reminder.java
    The naming style issue has been corrected, and the code has been reverted to the camel case naming convention that conforms to the class naming standards.

--- Final_Assignment/src/group/project/Reminder.java
+++ Final_Assignment/src/group/project/Reminder.java
@@ -9,31 +9,24 @@ package group.project;
  * @author 星飞
  */
 public class Reminder {
-    private int event_ID;
-    private String time;
+    private int eventId;
+    private String reminderTime;
     private String message;
     
     public Reminder(){
         
     }
-    
-    public int get_event_ID(){
-        return event_ID;
+    public Reminder(int eventId, String reminderTime, String message) {
+        this.eventId = eventId;
+        this.reminderTime = reminderTime;
+        this.message = message;
     }
-    public String get_time(){
-        return time;
-    }
-    public String get_message(){
-        return message;
-    }
-    
-    public void set_event_ID(int a){
-        event_ID=a;
-    }
-    public void set_time(String a){
-        time=a;
-    }
-    public void set_message(String a){
-        message=a;
-    }
+    public int getEventId() { return eventId; }
+    public void setEventId(int eventId) { this.eventId = eventId; }
+
+    public String getReminderTime() { return reminderTime; }
+    public void setReminderTime(String reminderTime) { this.reminderTime = reminderTime; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
